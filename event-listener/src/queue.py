c2d05be0f03b9b716660430d87a0d46323f68a68
Mahdi <mahdighadiriafzal@gmail.com>
2025-10-23 15:08:36 +0330

    Update queue.py

--- event-listener/src/queue.py
+++ event-listener/src/queue.py
@@ -5,7 +5,7 @@ import pika
 from .config import settings
 
 
-def publish_event(event_data: dict):
+async def publish_event(event_data: dict):
     connection = pika.BlockingConnection(pika.URLParameters(settings.rabbitmq_url))
     channel = connection.channel()
     channel.queue_declare(queue="events.detected", durable=True)
